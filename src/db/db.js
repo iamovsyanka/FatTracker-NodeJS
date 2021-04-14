8ee121d8028c9627ab02cb98e75f8b0dde865f2e
iamovsyanka <anakstkv@gmail.com>
2021-04-14 10:59:02 +0300

    feat: add parameters to env

--- src/db/db.js
+++ src/db/db.js
@@ -5,15 +5,21 @@ const logger = require('../logging/logger');
 if (!config.DB) return logger.error(new Error('The database is not set'));
 if (!config.USERNAME) return logger.error(new Error('The username to db is not set'));
 if (!config.PASSWORD) return logger.error(new Error('The password to db is not set'));
+if (!config.HOST) return logger.error(new Error('The host is not set'));
+if (!config.DIALECT) return logger.error(new Error('The dialect is not set'));
+if (!config.DB_PORT) return logger.error(new Error('The database port is not set'));
+if (config.POOL_MAX === undefined) return logger.error(new Error('The pool max is not set'));
+if (config.POOL_MIN === undefined) return logger.error(new Error('The pool min is not set'));
+if (config.POOL_IDLE === undefined) return logger.error(new Error('The pool idle is not set'));
 
 const sequelize = new Sequelize(config.DB, config.USERNAME, config.PASSWORD, {
   host: config.HOST,
   dialect: config.DIALECT,
   port: config.DB_PORT,
   pool: {
-    max: 100,
-    min: 0,
-    idle: 10000
+    max: config.POOL_MAX,
+    min: config.POOL_MIN,
+    idle: config.POOL_IDLE
   }
 });
 
@@ -35,6 +41,6 @@ console.log('Database loaded');
 console.log(models);
 
 module.exports = {
-  models: models,
-  sequelize: sequelize
+  models,
+  sequelize
 };
