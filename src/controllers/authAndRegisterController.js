f0bc4714a2b9473bb6da2186ede1aea3bb7b2eda
iamovsyanka <anakstkv@gmail.com>
2021-05-11 13:05:58 +0300

    refactor: delete promise

--- src/controllers/authAndRegisterController.js
+++ src/controllers/authAndRegisterController.js
@@ -4,46 +4,41 @@ const errMessages = require('../errors/errMessages');
 const error = require('../errors/appError');
 
 module.exports = {
+  //TODO 404
   async authUser(req, res) {
-    if (!(req.body.email && req.body.password)) {
-      return res.status(400).json(new error({status: 400, message: errMessages.BAD_DATA}));
+    try {
+      if (!(req.body.email && req.body.password)) {
+        return res.status(400).json(new error({ status: 400, message: errMessages.BAD_DATA }));
+      }
+
+      const token = await userService.login(req.body);
+      res.header(HTTP_HEADER_AUTHORIZATION, token).send({ token });
+    } catch (ex) {
+      return res.status(ex.status).json(new error({ status: ex.status, message: ex.message}));
     }
-    await userService.login(req.body)
-      .then((token) => {
-        res
-          .header(HTTP_HEADER_AUTHORIZATION, token)
-          .send({ token });
-      })
-      .catch((err) => {
-        console.error(err.message);
-      });
   },
 
   async registerUser(req, res) {
-    if (!(req.body.email && req.body.password && req.body.name)) {
-      return res.status(400).json(new error({status: 400, message: errMessages.BAD_DATA}));
+    try {
+      if (!(req.body.email && req.body.password && req.body.name)) {
+        return res.status(400).json(new error({ status: 400, message: errMessages.BAD_DATA }));
+      }
+
+      const newUser = await userService.registration(req.body);
+      res.end(JSON.stringify(newUser));
+    } catch (ex) {
+      return res.status(ex.status).json(new error({ status: 500, message: ex.message}));
     }
-    await userService.registration(req.body)
-      .then((result) => {
-        res.type('json');
-        res.end(JSON.stringify(result));
-      })
-      .catch((err) => {
-        console.error(err.message);
-      });
   },
 
   async verifyAccount(req, res) {
-    console.log(req.query);
-    if (req.query.token) {
-      await userService.verifyAccount(req.query.token)
-        .then((result) => {
-          res.type('json');
-          res.end(JSON.stringify(result));
-        })
-        .catch((err) => {
-          console.error(err.message);
-        });
+    try {
+      if (req.query.token) {
+        const user = await userService.verifyAccount(req.query.token);
+        res.end(JSON.stringify(user));
+      }
+    } catch (ex) {
+      return res.status(ex.status).json(new error({ status: ex.status, message: ex.message}));
     }
   }
 };
