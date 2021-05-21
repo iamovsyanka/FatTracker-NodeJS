5c286d8ab182630fd8ecba4e8957764e412ca56d
iamovsyanka <anakstkv@gmail.com>
2021-05-22 02:56:30 +0300

    refactor: change method

--- src/routers/dayRouter.js
+++ src/routers/dayRouter.js
@@ -4,7 +4,7 @@ const dayController = require('../controllers/dayController');
 const dayRouter = express.Router();
 
 dayRouter.post('/get', dayController.get);
-dayRouter.post('/getAllByUser', dayController.getAllByUser);
+dayRouter.get('/getAllByUser', dayController.getAllByUser);
 dayRouter.post('/getSinceDate', dayController.getSinceDate);
 dayRouter.post('/add', dayController.add);
 
